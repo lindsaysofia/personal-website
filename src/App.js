549b30e58cb44edf4a3ceff4b02888a713296c49
Lindsay Lopez <lopez95@berkeley.edu>
2021-12-31 15:03:50 -0800

    created Home section and styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import '../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="Home" id="home">
+      <h1>Hi, I'm Lindsay</h1>
+      <p className="image-credit">Photo by <a href="https://unsplash.com/@andyjh07?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andy Holmes</a> on <a href="https://unsplash.com/s/photos/night-sky?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+    </div>
+  );
+}
+
+export default Home;
