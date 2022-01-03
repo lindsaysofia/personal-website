b64504966711dd7ddcce22af3140a54bc73e364e
Lindsay Lopez <lopez95@berkeley.edu>
2022-01-03 11:15:43 -0800

    update home bg and add typing effect

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ function Home() {
   return (
     <div className="Home" id="home">
       <h1>Hi, I'm Lindsay.</h1>
-      <p className="image-credit">Photo by <a href="https://unsplash.com/@andyjh07?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Andy Holmes</a> on <a href="https://unsplash.com/s/photos/night-sky?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+      <p className="image-credit">Photo by <a href="https://unsplash.com/@yogidan2012?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Daniele Levis Pelusi</a> on <a href="https://unsplash.com/s/photos/purple-fire?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  </p>
     </div>
   );
 }
