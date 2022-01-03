ac054d79ef1bc92ff223bf1bfd165789eba50cfa
Lindsay Lopez <lopez95@berkeley.edu>
2022-01-03 14:46:19 -0800

    change typewriter effect for home

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const githubDemo = 'https://lindsaysofia.github.io/';
 
   const projects = [{project: 'etch-a-sketch', name: 'Etch-A-Sketch', description: ''}, {project: 'tic-tac-toe', name: 'Tic-Tac-Toe', description: ''}, {project: 'library', name: "Library", description: ''}, {project: 'todo-list', name: 'Todo List', description: ''}, {project: 'restaurant-page', name: 'Restaurant Page', description: ''}, {project: 'calculator', name: 'Calculator', description: ''}, {project: 'cv-project', name: 'CV Project', description: ''}, {project: 'memory-game', name: 'Memory Game', description: ''}, {project: 'photo-tagging', name: 'Photo Tagging', description: ''}, {project: 'shopping-cart', name: 'Shopping Cart', description: ''}, {project: 'battleship', name: 'Battleship', description: ''}];
+
   return (
     <div className="App">
       <Navigation />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,52 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import '../styles/Home.css';
 
+/* From https://css-tricks.com/snippets/css/typewriter-effect/ */
+
 function Home() {
+  document.addEventListener('DOMContentLoaded',(event) => {
+    // array with texts to type in typewriter
+    const dataText = ["Hi, I'm Lindsay.", "Hola, soy Lindsay.", "Welcome!", "Thanks for stopping by!", "Gracias por visitar mi página :)"];
+    
+    // type one text in the typwriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+      // chekc if text isn't finished yet
+      if (i < (text.length)) {
+        // add next character to h1
+       document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+  
+        // wait for a while and call this function again for next character
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 100);
+      }
+      // text finished, call callback if there is a callback function
+      else if (typeof fnCallback == 'function') {
+        // call callback after timeout
+        setTimeout(fnCallback, 700);
+      }
+    }
+    // start a typewriter animation for a text in the dataText array
+     function StartTextAnimation(i) {
+       if (typeof dataText[i] == 'undefined'){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 20000);
+       }
+       // check if dataText[i] exists
+      if (i < dataText[i].length) {
+        // text exists! start typewriter animation
+       typeWriter(dataText[i], 0, function(){
+         // after callback (and whole text has been animated), start next text
+         StartTextAnimation(i + 1);
+       });
+      }
+    }
+    // start the text animation
+    StartTextAnimation(0);
+  });
+
   return (
     <div className="Home" id="home">
       <h1>Hi, I'm Lindsay.</h1>
