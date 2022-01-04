d8771d266d44cf971843c867a0cfdfa8e3d413ad
Lindsay Lopez <lopez95@berkeley.edu>
2022-01-03 16:16:30 -0800

    elaborate in About section and import handwriting font to use

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,11 @@ function About() {
     <div className="About" id="about">
       <h2 className="section-header"><span className="color-main">{"<"}</span>About Me <span className="color-main">{"/>"}</span></h2>
       <div className="About-content">
-        <h3>A Short Haiku to Describe Myself:</h3>
-        <p>A lifelong learner,</p>
-        <p>I like to push my limits</p>
-        <p>and unearth knowledge.</p>
-        <p>In all seriousness, I am passionate about problem-solving. Software engineering has captured my interest for years, and I'd love to pursue a career in it.</p>
+        <p>A short haiku to describe myself:</p>
+        <p className="About-haiku">A lifelong learner,</p>
+        <p className="About-haiku">I like to push my limits</p>
+        <p className="About-haiku">and unearth knowledge.</p>
+        <p>In all seriousness, I’m passionate about solving problems and I want a career in software development because that challenges me and forces me to think outside the box. I have consistently gravitated towards web development and I truly enjoy it. I find it exciting to sit down with my whiteboard and plan out my projects. I'm elated when I finally start coding (and come across problems I didn't initally think about)- there’s always so much to learn! My goal is to start in front-end and eventually learn more of the back-end in order to become a full-stack developer. However, I am open to different paths- I wouldn't want an intriguing oppurtunity to pass me by!</p>
       </div>
     </div>
   );
