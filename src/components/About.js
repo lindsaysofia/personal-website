16df1324fc205d6ce021b04594d574da3f57f288
Lindsay Lopez <lopez95@berkeley.edu>
2022-01-31 12:12:18 -0800

    edit about me and change project order

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ function About() {
       <div className="About-content">
         <p>A short haiku to describe myself:</p>
         <p className="About-haiku">A lifelong learner,</p>
-        <p className="About-haiku">I like to push my limits</p>
-        <p className="About-haiku">and unearth knowledge.</p>
-        <p>In all seriousness, I’m passionate about solving problems and I want a career in software development because that challenges me and forces me to think outside the box. I have consistently gravitated towards web development and I truly enjoy it. I find it exciting to sit down with my whiteboard and plan out my projects. I'm elated when I finally start coding (and come across problems I didn't initally think about)- there’s always so much to learn! My goal is to start in front-end and eventually learn more of the back-end in order to become a full-stack developer. However, I am open to different paths- I wouldn't want an intriguing oppurtunity to pass me by!</p>
+        <p className="About-haiku">I always give it my all</p>
+        <p className="About-haiku">and push my limits.</p>
+        <p>In all seriousness, I’m passionate about solving problems and I love software development because it challenges me and forces me to think outside the box. One of my favorite aspects of the development process is to sit down with my whiteboard and plan out my projects. I'm elated when I finally start coding- there’s always so much to learn!</p>
       </div>
     </div>
   );
