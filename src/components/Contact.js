1edc4f2b4953301ed0358512fa82de1f0a2450f3
Lindsay Lopez <lopez95@berkeley.edu>
2022-02-01 16:47:42 -0800

    updated resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
       <a title="Email" href="mailto:lindsaysofialopez@gmail.com"><i className="fas fa-envelope-open-text"></i></a>
       <a title="Phone" href="tel:+17602167222"><i className="fas fa-phone"></i></a>
       <a title="GitHub" href="https://github.com/lindsaysofia" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a>
-      <a title="Resume" href="https://docs.google.com/document/d/e/2PACX-1vRuLRe4Xb9A1dSzz9mvgQZVpdmo9AP6a_sSL0WGG3CkzONXedKqJ3AUpI3A-8sGEwSogTA-MvNzKWvV/pub" target="_blank" rel="noreferrer"><i className="fas fa-file"></i></a>
+      <a title="Resume" href="https://drive.google.com/file/d/1tTiTqdHH2eKSvr3xtg-K_4xyxnjuBo5d/view?usp=sharing" target="_blank" rel="noreferrer"><i className="fas fa-file"></i></a>
     </address>
   );
 }
